232f40887244efbbc1bbb4474d10cb99e92d6ddc
Babul Raj Singh <95072470+ba6ul@users.noreply.github.com>
2025-06-03 10:42:29 +0530

    Update bubble_sorting.py

--- bubble_sorting.py
+++ bubble_sorting.py
@@ -11,10 +11,10 @@
 #
 # -------------------- Bubble Sort Implementation --------------------
 array = [2, 4, 23, 54, 75, 234, 46, 2] 
-n = len(array)
+n = len(array)-1
 
-for i in range(n-1):
-    for j in range(n-i-1):
+for i in range(n):
+    for j in range(n-i):
         if array[j] > array[j+1]:
             array[j], array[j+1] = array[j+1], array[j]
 
