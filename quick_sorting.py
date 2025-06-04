a58c806d82ab348f8d60699ceb7bb98f102e08c1
Babul Raj Singh <95072470+ba6ul@users.noreply.github.com>
2025-06-04 22:36:53 +0530

    Update quick_sorting.py

--- quick_sorting.py
+++ quick_sorting.py
@@ -1,19 +1,28 @@
-#first we pick pivot.
+# First, we pick the pivot.
 array = [98, 3, 72, 56, 89, 12, 41, 67, 23, 1, 
          95, 83, 11, 47, 34, 28, 79, 64, 50, 9,
          102, 45, 38, 76, 88, 24, 92, 81, 15, 61, 
          110, 5, 99, 31, 18, 70, 55, 42, 26, 8]
-x = len(array)
-p = array[x//2] #Middle of the array
 
-print(p)
-n = len(array)
-small = []
-big = []
-for i in range (0,n):
-    if array[i] < p:
-        small.append(array[i])
-    else:
-        big.append(array[i])
+def quicksort(array):
+    if len(array) <= 1: #u sptuid
+        return array
 
-print(small,big)
+    n = len(array)
+    p = array[n//2] 
+
+    small = []
+    big = []
+    
+    for i in range(n):
+        if array[i] < p:
+            small.append(array[i])
+        elif array[i] > p:
+            big.append(array[i])
+
+    return quicksort(small) + [p] + quicksort(big)
+
+
+newarray = quicksort(array)
+
+print("Sorted Array:", newarray)
