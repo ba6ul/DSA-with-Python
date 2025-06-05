619e41cf1c6d6334bd852adc900d65d7879474b0
Babul Raj Singh <95072470+ba6ul@users.noreply.github.com>
2025-06-05 20:53:49 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,12 @@
 array = [3, 8, 12, 17, 21, 27, 33, 39, 44, 50]
 
 target = 24
+
+
+high = len(array)
+mid = high // 2
+
+if array[mid] < target:
+    for i in range(mid,high):
+        if array[i] == target:
+            print(i)
